ea4f3bb51bceca1461431fbd70bc42cbbd306f83
Georgina Pacey <paceygeorgina@gmail.com>
2018-06-10 14:22:43 +0200

    animation rotate

--- js/game.js
+++ js/game.js
@@ -46,7 +46,8 @@ Game.prototype.moveAll = function() {
 Game.prototype.checkGameOver = function() {
 
     if (this.obstacleCollection.checkCollisions(this.player)) {
-        this.player.img.frameIndex = 2;
+        this.player.img.frames = 1;
+        this.player.img.src = "img/bubble-die.png";
         setTimeout(function() { 
             this.gameOver();
         }.bind(this), 100);
@@ -77,12 +78,12 @@ Game.prototype.clear = function() {
             //check if collided number is the first one in the list
             if (i == 0){
                 //later we will use this to change emotion
-                this.player.img.frameIndex = 1;
+                this.player.img.frames = 7;
+                this.player.img.src = "img/happy-bubble-sprite.png";
                 this.delay();
                 this.numberCollection.numbers.splice(i,1);
                 this.numberCollection.numbersCollected++;
                 this.score++;    
-                console.log(this.score);            
             }
             
         }
@@ -94,8 +95,9 @@ Game.prototype.clear = function() {
 
  Game.prototype.delay = function() {
     setTimeout(function() { 
-        this.player.img.frameIndex = 0;
-    }.bind(this), 500);
+        this.player.img.frames = 1;
+        this.player.img.src = "img/bubble.png";
+    }.bind(this), 700);
 } 
 Game.prototype.nextLevel = function() {
     if (this.numberCollection.numbers.length <= 0) {
